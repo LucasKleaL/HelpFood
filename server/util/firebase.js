f42d9fc1037e299c42ba6590deb3227f8e587c94
Lucas Kusman Leal <lucaskleal222@outlook.com>
2022-04-23 13:12:04 -0300

    Init Firebase
    -Adicionado inicialização de conexão com o Firebase a partir do Node server.

---
+++ server/util/firebase.js
@@ -0,0 +1,23 @@
+const firebase = require("firebase");
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCrjJnz15tCvZSiUcqHtG6VetMYXbePyFI",
+  authDomain: "helpfood-29ce0.firebaseapp.com",
+  projectId: "helpfood-29ce0",
+  storageBucket: "helpfood-29ce0.appspot.com",
+  messagingSenderId: "561309052030",
+  appId: "1:561309052030:web:de8b1a8b587300b61cfc2f",
+  measurementId: "G-Y5SW3SNE08"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+module.exports = { firebase };
