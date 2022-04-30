fbab55500e1386370e9cdcc777403c505192a6d2
Lucas Kusman Leal <lucaskleal222@outlook.com>
2022-04-29 21:16:55 -0300

    CadastroUsuario 0.2
    -Adicionado funcionalidade para pegar os valores de input e setar em variáveis.
    -Adicionado função para mandar post do form para o node server.
    -Adicionado dependencia cors no node server.
    -Adicionado litening do user/add post no node server.

--- client/src/pages/UserRegister.jsx
+++ client/src/pages/UserRegister.jsx
@@ -2,8 +2,27 @@ import { React, useState } from "react";
 import { Container, Grid, Button, Box, TextField, Select } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import MaterialButtonTheme from "./../themes/MaterialButtonTheme";
+import Axios from "axios";
 
 function UserRegister () {
+
+    const [name, setName] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [email, setEmail] = useState("");
+    const [ongName, setOngName] = useState("");
+    const [password, setPassword] = useState("");
+    const [retryPassword, setRetryPassword] = useState("");
+
+    function addUser() {
+        Axios.post("http://localhost:3001/user/add", {
+            name: name,
+            cpf: cpf,
+            email: email,
+            ongName: ongName,
+            password: password,
+            retryPassword: retryPassword
+        });
+    }
 
     const BoxStyle = {
         backgroundColor: "var(--white-background)",
@@ -37,15 +56,33 @@ function UserRegister () {
                 <div>
                     <Box style={BoxStyle}>
                         <div>
-                            <TextField variant="outlined" type="text" label="Nome" style={{...TextFieldSytle,...{marginTop: "2rem"}}} />
-                            <TextField variant="outlined" label="CPF" type="text" style={TextFieldSytle} />
-                            <TextField variant="outlined" label="Email" type="EMAIL" style={TextFieldSytle} />
-                            <TextField variant="outlined" label="Nome da ONG" type="text" style={TextFieldSytle} />
-                            <TextField variant="outlined" label="Senha" type="password" style={TextFieldSytle} />
-                            <TextField variant="outlined" label="Confirme a senha" type="password" style={TextFieldSytle} />
+                            <TextField variant="outlined" type="text" label="Nome" 
+                            style={{...TextFieldSytle,...{marginTop: "2rem"}}}
+                            onChange={(e) => {setName(e.target.value)}}
+                            />
+                            <TextField variant="outlined" label="CPF" type="text" 
+                            style={TextFieldSytle}
+                            onChange={(e) => {setCpf(e.target.value)}}
+                            />
+                            <TextField variant="outlined" label="Email" type="EMAIL" 
+                            style={TextFieldSytle}
+                            onChange={(e) => {setEmail(e.target.value)}}
+                            />
+                            <TextField variant="outlined" label="Nome da ONG" type="text" 
+                            style={TextFieldSytle}
+                            onChange={(e) => {setOngName(e.target.value)}}
+                            />
+                            <TextField variant="outlined" label="Senha" type="password" 
+                            style={TextFieldSytle}
+                            onChange={(e) => {setPassword(e.target.value)}}
+                            />
+                            <TextField variant="outlined" label="Confirme a senha" type="password" 
+                            style={TextFieldSytle}
+                            onChange={(e) => {setRetryPassword(e.target.value)}}
+                            />
                         </div>
                         <div>
-                            <Button variant="contained" color="primary" style={RegisterButtonStyle}>Cadastrar</Button>
+                            <Button variant="contained" color="primary" style={RegisterButtonStyle} onClick={addUser}>Cadastrar</Button>
                         </div> 
                     </Box>
                 </div>
