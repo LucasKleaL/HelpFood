ac8fc5445bbc2f22e750e8fa0b073b533a70ed2f
tiagofelipemuller <tiagofelipemuller@gmail.com>
2022-05-17 21:48:58 -0300

    criando /dashboard no Routes.jsx

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import UserRegister from "./pages/UserRegister";
 import LandingPage from "./pages/LandingPage";
 import CompanyRegister from "./pages/CompanyRegister";
+import DonationDashboard from "./pages/DonationDashboard";
 
 const Routes = () => {
     return (
@@ -9,6 +10,7 @@ const Routes = () => {
             <Route exact={true} path="/" component={LandingPage} />
             <Route exact={true} path="/user/add" component={UserRegister} />
             <Route exact={true} path="/company/add" component={CompanyRegister} />
+            <Route exact={true} path="/dashboard" component={DonationDashboard} />
         </BrowserRouter>
     )
 }
