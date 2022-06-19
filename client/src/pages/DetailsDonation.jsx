d5b91b39e54cc13feea9b95b757253facdac5626
Tiago Muller <=>
2022-06-18 23:00:17 -0300

    json aparecendo no html

--- client/src/pages/DetailsDonation.jsx
+++ client/src/pages/DetailsDonation.jsx
@@ -1,50 +1,42 @@
 import {useEffect, useState} from "react";
 
-function DetailsDonation(){
-const id = window.location.search;
+function DetailsDonation() {
+    const id = window.location.search;
+    const urlParam = new URLSearchParams(id);
+    const paramId = urlParam.get("donationId");
 
-const urlParam = new URLSearchParams(id);
+    const [donations, setDonation] = useState([]);
 
-const paramId = urlParam.get("donationId");
-
-console.log(paramId);
-console.log("caiu")
-    // const [donations, setDonation] = useState([]);
-
-    // useEffect(() => {
-    //     fetch("http://localhost:3001/donation/getDonationById/:" + donationId, {
-    //         method: "GET",
-    //         headers: {
-    //             Accept: "application/json",
-    //             "Content-Type": "application/json"
-    //         }
-    //     })
-    //         .then((res) => res.json())
-    //         .then(
-    //             (result) => {
-    //                 setDonation(result)
-    //             }
-    //         );
-    // }, [])
+    useEffect(() => {
+        fetch("http://localhost:3001/donation/getDonationById/" + paramId, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setDonation(result)
+                }
+            );
+    }, [])
 
     return (
         <div>
-            {/*{donations.map((donation) => (*/}
-            {/*    <div>{*/}
-            {/*        donation.Address + ", " +*/}
-            {/*        donation.BusinessDonor + ", " +*/}
-            {/*        donation.Description + ", " +*/}
-            {/*        donation.Name + ", " +*/}
-            {/*        donation.Quantity + ", " +*/}
-            {/*        donation.ShelfLife + ", " +*/}
-            {/*        donation.TypeFood + ", " +*/}
-            {/*        donation.Weight*/}
-            {/*    }</div>*/}
-
-            {/*))}*/}
-
-            <h1>teste</h1>
-            {paramId}
+            {donations.map((donation) => (
+                <div>{
+                    donation.Address + ", " +
+                    donation.BusinessDonor + ", " +
+                    donation.Description + ", " +
+                    donation.Name + ", " +
+                    donation.Quantity + ", " +
+                    donation.ShelfLife + ", " +
+                    donation.TypeFood + ", " +
+                    donation.Weight
+                }</div>
+            ))}
         </div>
     )
 
