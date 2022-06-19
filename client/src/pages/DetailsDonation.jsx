546a7fefd58c9f13e9c695ee57ca96e437641685
Tiago Muller <=>
2022-06-18 23:28:16 -0300

    adicionando card

--- client/src/pages/DetailsDonation.jsx
+++ client/src/pages/DetailsDonation.jsx
@@ -1,4 +1,11 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+import {Container, Grid} from "@mui/material";
+import Divider from "@mui/material/Divider";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
+import ThemeComponent from "../components/ThemeComponent";
 
 function DetailsDonation() {
     const id = window.location.search;
@@ -6,6 +13,14 @@ function DetailsDonation() {
     const paramId = urlParam.get("donationId");
 
     const [donations, setDonation] = useState([]);
+
+    const themeComponent = new ThemeComponent();
+    const theme = themeComponent.getActualTheme();
+
+    const ButtonStyle = {
+        width: "50%",
+        fontWeigth: "solid",
+    }
 
     useEffect(() => {
         fetch("http://localhost:3001/donation/getDonationById/" + paramId, {
@@ -25,18 +40,86 @@ function DetailsDonation() {
 
     return (
         <div>
-            {donations.map((donation) => (
-                <div>{
-                    donation.Address + ", " +
-                    donation.BusinessDonor + ", " +
-                    donation.Description + ", " +
-                    donation.Name + ", " +
-                    donation.Quantity + ", " +
-                    donation.ShelfLife + ", " +
-                    donation.TypeFood + ", " +
-                    donation.Weight
-                }</div>
-            ))}
+            {
+                donations.length === 0 ?
+                    <Container maxWidth="lg">
+                        <Grid container justifyContent="center" paddingTop={15}>
+                            <h1 style={{color: themeComponent.getTypographyContrastColor(theme)}}>Nenhuma doação
+                                disponível no momento. 🙁</h1>
+                        </Grid>
+                    </Container>
+                    :
+                    <Container maxWidth="lg">
+                        <Grid>
+                            <Grid container spacing={1.3} justifyContent="center" padding={3} style={{}}>
+                                <Grid item>
+                                    <h3 style={{
+                                        fontSize: "60px",
+                                        color: themeComponent.getTypographyColor(theme)
+                                    }}>Detalhes da Doação</h3>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Divider style={{
+                            backgroundColor: themeComponent.getTypographyContrastColor(theme),
+                            opacity: "25%"
+                        }}/>
+                        <Grid container spacing={1.3} padding={3} justifyContent="center">
+                            {donations.map((donation) => (
+                                <Grid item key={donation.Id}>
+                                    <Card sx={{
+                                        maxWidth: 315,
+                                        minWidth: 315,
+                                        backgroundColor: themeComponent.getCardBackgroundColor(theme)
+                                    }}>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            image="https://static.vecteezy.com/ti/vetor-gratis/p1/2554852-seamless-pattern-with-food-on-dark-blue-background-gr%C3%A1tis-vetor.jpg"
+                                            alt="HelpFood img"
+                                        />
+                                        <div>
+                                            <Typography variant="body2" sx={{
+                                                color: themeComponent.getTypographyColor(theme),
+                                                float: "right",
+                                                paddingRight: "4px"
+                                            }}>
+                                                Validade: {donation.ShelfLife}
+                                            </Typography>
+                                        </div>
+                                        <CardContent>
+                                            <Typography className="nameCard" gutterBottom variant="h6" component="div"
+                                                        sx={{color: themeComponent.getTypographyColor(theme)}}>
+                                                {donation.Name}
+                                            </Typography>
+                                            <Typography className="description" variant="body2"
+                                                        sx={{color: themeComponent.getTypographyColor(theme)}}>
+                                                {donation.Description}
+                                            </Typography>
+
+                                        </CardContent>
+
+                                        <CardContent>
+                                            <Typography variant="body2" sx={{
+                                                float: "right",
+                                                paddingRight: "4px",
+                                                color: themeComponent.getTypographyColor(theme)
+                                            }}>
+                                                {donation.Quantity} un.
+                                            </Typography>
+                                            <Typography variant="body2"
+                                                        sx={{color: themeComponent.getTypographyColor(theme)}}>
+                                                Bairro: {donation.District}
+                                            </Typography>
+                                            <input type={"hidden"} id={donation.Id} value={donation.Id}></input>
+                                        </CardContent>
+                                        <Divider/>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Container>
+            }
         </div>
     )
 
